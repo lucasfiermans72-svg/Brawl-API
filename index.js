a5adbd9f7ff394a2d9abee805905d68578d4ac23
lucasfiermans72-svg <lucasfiermans72@gmail.com>
2026-03-25 19:29:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/api/player", async (req, res) => {
       `https://api.brawlstars.com/v1/players/%23${tag}`,
       {
         headers: {
-          Authorization: `Bearer ${eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjkyYjMyNjZhLWIzZGItNDA2Mi1iZTgwLTVjZTk4MjU3YWQwYyIsImlhdCI6MTc3NDQ2MjMzMiwic3ViIjoiZGV2ZWxvcGVyL2MyOGU4MWU3LTc1ODktZTExYy0wYTQwLTkyNjVjNWMxY2EyMCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiOTQuMjI3LjEwNy4xMzAiXSwidHlwZSI6ImNsaWVudCJ9XX0.0XFKGeykwYLGNwQn1ErBx1aeYpPpfFVjXgVMoSBJzG6vzTELy2O9WgLPLF30n0tHXUH0YycZanbyDCS4HoZz4w}`,
+          Authorization: `Bearer ${process.env.API_KEY}`,
         },
       }
     );
